7759193ab6ce447e3e598a458407fa80d910c39e
VickArmand <victormaina1962@gmail.com>
2024-03-20 02:05:10 +0300

    adding media queries

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,2 +1,13 @@
 const txtyear = document.getElementById('current-year');
 txtyear.textContent = new Date().getFullYear();
+const nav = document.getElementById('nav');
+function hideNav(){
+    if(nav.style.display == "block")
+    {
+        nav.style.display = "none";
+    }
+    else
+    {
+        nav.style.display = "block";
+    }
+}
