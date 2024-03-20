642621cc29d76df9021478ca82e26866bb2e5d12
VickArmand <victormaina1962@gmail.com>
2024-03-20 14:32:15 +0300

    more styling

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,12 +2,5 @@ const txtyear = document.getElementById('current-year');
 txtyear.textContent = new Date().getFullYear();
 const nav = document.getElementById('nav');
 function hideNav(){
-    if(nav.style.display == "block")
-    {
-        nav.style.display = "none";
-    }
-    else
-    {
-        nav.style.display = "block";
-    }
+    nav.classList.toggle("active");
 }
